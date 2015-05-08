f5bd07b186917d837e4df717fd4f56e780032aff
developercyrus <developer.cyrus@gmail.com>
2015-05-08 09:29:18 +0800

    @i3300

--- src/main/java/javaee/jpa/openjpa/example1/Client.java
+++ src/main/java/javaee/jpa/openjpa/example1/Client.java
@@ -1,12 +1,12 @@
 package javaee.jpa.openjpa.example1;
 
 public class Client {
-	public static String getCustomerName() {
-		CustomerDao dao = new CustomerDao();
-		return dao.findCustomerById(1).getName();
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(getCustomerName());
-	}	
+    public static String getCustomerName() {
+        CustomerDao dao = new CustomerDao();
+        return dao.findCustomerById(1).getName();
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(getCustomerName());
+    }	
 }
--- src/main/java/javaee/jpa/openjpa/example1/CustomerDao.java
+++ src/main/java/javaee/jpa/openjpa/example1/CustomerDao.java
@@ -8,14 +8,14 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 public class CustomerDao {
-	public Customer findCustomerById(int id) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("javaee.jpa.openjpa.example1.Customer");
+    public Customer findCustomerById(int id) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("javaee.jpa.openjpa.example1.Customer");
         EntityManager em = emf.createEntityManager();
 
         String q = "SELECT c FROM Customer AS c WHERE c.id = ?1";
         Query query = (Query) em.createQuery(q);
         query.setParameter(1, new Integer(id));
         List<?> l =  query.getResultList();
-       	return (Customer)l.get(0);
-	}
+        return (Customer)l.get(0);
+    }
 }
